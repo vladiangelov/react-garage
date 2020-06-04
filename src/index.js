4aa6336950bcb77a1ac3f197c48615040b01d835
Vladi Angelov <vladislav_angelov@yahoo.com>
2020-06-04 20:13:08 +0100

    Created a simple react router / redux boilerplate

--- src/index.js
+++ src/index.js
@@ -2,14 +2,26 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import store from './app/store';
+import { createStore, applyMiddleware } from 'redux';
 import { Provider } from 'react-redux';
 import * as serviceWorker from './serviceWorker';
+import rootReducer from './reducers/rootReducer';
+
+import { BrowserRouter as Router, Route, Redirect, Switch } from 'react-router-dom';
+import { createBrowserHistory as history } from 'history';
+
+const initialState = {};
+
+const store = createStore(rootReducer, initialState);
 
 ReactDOM.render(
   <React.StrictMode>
     <Provider store={store}>
-      <App />
+      <Router history={history}>
+       <Switch>
+         <Route path="/" component={App} />
+       </Switch>
+     </Router>
     </Provider>
   </React.StrictMode>,
   document.getElementById('root')
